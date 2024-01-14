962a47e377e97e0e87c18936464ee1e3445503fb
Olha Bezkorovaina <146225947+Olga-art-13@users.noreply.github.com>
2024-01-14 21:13:14 +0300

    program

--- src/route/index.js
+++ src/route/index.js
@@ -497,7 +497,7 @@ router.get('/person', function (req, res) {
 router.get('/program', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('program', {
-    layout: program,
+    layout: 'program',
     program: {
       excursion: {
         name: 'Cultural Tour',
